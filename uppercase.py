e3a9a24a7e7d587c96c9d3574735444c5cd2608d
Catalin Iacob <iacobcatalin@gmail.com>
2012-09-22 17:13:53 +0200

    Implement response with uppercase received data

--- uppercase.py
+++ uppercase.py
@@ -4,6 +4,9 @@ class UpperProtocol(protocol.Protocol):
     def connectionMade(self):
         self.transport.write('Hi! Send me some text to convert to uppercase\n')
 
+    def dataReceived(self, data):
+        self.transport.write(data.upper())
+
 factory = protocol.ServerFactory()
 factory.protocol = UpperProtocol
 
