0c77600198733b1edb502bcf04b8e9bd5284eae7
Catalin Iacob <iacobcatalin@gmail.com>
2012-09-22 17:14:14 +0200

    Log connection errors

--- uppercase.py
+++ uppercase.py
@@ -4,6 +4,9 @@ class UpperProtocol(protocol.Protocol):
     def connectionMade(self):
         self.transport.write('Hi! Send me some text to convert to uppercase\n')
 
+    def connectionLost(self, reason):
+        print 'Lost connection from {0.host}:{0.port}'.format(self.transport.getPeer())
+
     def dataReceived(self, data):
         self.transport.write(data.upper())
 
