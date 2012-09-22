3a5bbd6ed818cd9d15bbfc4b4e3f17ea67fe6b6c
Catalin Iacob <iacobcatalin@gmail.com>
2012-09-22 21:07:52 +0200

    Modify proxy to make the callbacks methods of the protocol class

--- proxy.py
+++ proxy.py
@@ -5,23 +5,23 @@ from twisted.web.client import getPage
 import time
 
 class ProxyProtocol(basic.LineReceiver):
+    def gotPage(self, data, line, start):
+        print 'Fetched {} in {} sec'.format(line, time.time() - start)
+        self.transport.write(data)
+
+    def errGettingPage(self, reason, line):
+        print 'Error while fetching {}: {}'.format(line, reason.getErrorMessage())
+
     def lineReceived(self, line):
         if not line.startswith('http://'):
             return
 
         start = time.time()
-
         print 'Fetching {}'.format(line)
-        def gotPage(data):
-            print 'Fetched {} in {} sec'.format(line, time.time() - start)
-            self.transport.write(data)
-
-        def errGettingPage(reason):
-            print 'Error while fetching {}: {}'.format(line, reason.getErrorMessage())
-
         d = getPage(line)
-        d.addCallback(gotPage)
-        d.addErrback(errGettingPage)
+
+        d.addCallback(self.gotPage, line, start)
+        d.addErrback(self.errGettingPage, line)
 
 if __name__ == '__main__':
     factory = protocol.ServerFactory()
